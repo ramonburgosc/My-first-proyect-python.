7b12aab68a7b3463887dbb7051b86beea5cab57d
ramonburgosc <geglin05@gmail.com>
2026-02-25 15:49:22 -0500

    Uploading my first salary caalculator.

---
+++ test.py
@@ -0,0 +1,7 @@
+#Pedimos los datos
+pago_por_hora=input("cuanto quieres ganar por hora en dolares?")
+horas_al_mes=input("cuantas horas vas a trabajr al mes?")
+#Convertimos el texto a numero para poder multiplicar 
+total= float (25) * float (160) 
+#Mmostramos el resultado uniendo las partes con el signo +
+print ("tu sueldo mensual estimado seria de: " + str(total) +" dolares")
